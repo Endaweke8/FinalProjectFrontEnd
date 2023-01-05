889eff1c22560d08bc781a09d84b65ee02ac2396
Endaweke8 <endaweke1234@gmail.com>
2023-01-06 01:56:53 +0300

    After Serache functionality

--- src/components/NavigationVue.vue
+++ src/components/NavigationVue.vue
@@ -382,37 +382,16 @@
             class="py-2 px-2 text-base font-regular primary-color"
             >Stocks</router-link
           >
-          <form class="flex items-center ">
-              <label for="voice-search" class="sr-only">Search</label>
-              <div class="relative ">
-                <div
-                  class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
-                >
-                  <svg
-                    aria-hidden="true"
-                    class="w-5 h-auto text-gray-500 dark:text-gray-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
-                <input
+          <span> <input
                   type="text"
                   id="voice-search"
                   v-model="query"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Search..."
                   required
-                />
-                <span  @click="searchProducts(query)" class="bg-blue-600 p-1 rounded">Search</span>
-                </div>
-                </form>
+                /></span>
+                <button  @click="searchProducts(query)" class="bg-blue-600 p-1 rounded">Search</button>
+        
         </div>
 
         <!-- Mobile menu button -->
@@ -634,7 +613,7 @@ class=" max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray
              :pages="pageCount"
              :range-size="1"
              active-color="#337aff"
-             @update:modelValue="getProducts"
+             @update:modelValue="searchProducts"
          />
      </div>
     </div>
